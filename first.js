4bc042483ef3f3ea02feba4a39b7b16e6e95bceb
quangnguyenduc123 <46807965+quangnguyenduc123@users.noreply.github.com>
2021-09-10 16:39:55 +0700

    model schema in api gateway

--- first.js
+++
@@ -1,7 +0,0 @@
-var a = 'hello';
-var b = 5;
-console.log(a);
-var c = [];
-c.push(a);
-c.push(b);
-console.log(c);
--- generics.ts
+++
@@ -1,7 +0,0 @@
-function returnKeys<T extends object>(arg: T){
-    console.log(Object.keys(arg))
-    return arg
-}
-returnKeys({
-    abc: 'def'
-})
